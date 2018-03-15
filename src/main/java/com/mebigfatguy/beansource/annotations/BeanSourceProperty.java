7ad2273168b07a187a32d5dbae50d18006e41e6d
Dave Brosius <dbrosius@mebigfatguy.com>
2018-03-14 22:51:25 -0400

    make target only methods

--- src/main/java/com/mebigfatguy/beansource/annotations/BeanSourceProperty.java
+++ src/main/java/com/mebigfatguy/beansource/annotations/BeanSourceProperty.java
@@ -21,7 +21,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.METHOD, ElementType.TYPE })
+@Target({ ElementType.METHOD })
 public @interface BeanSourceProperty {
 
     enum Type {
