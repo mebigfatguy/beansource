d4f268ee454ca6dceb2f6c93740788a818f1fb13
Dave Brosius <dbrosius@mebigfatguy.com>
2019-02-05 23:12:14 -0500

    Happy New Year

--- src/main/java/com/mebigfatguy/beansource/annotations/BeanSourceProperty.java
+++ src/main/java/com/mebigfatguy/beansource/annotations/BeanSourceProperty.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2018 Dave Brosius
+ * Copyright 2005-2019 Dave Brosius
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
